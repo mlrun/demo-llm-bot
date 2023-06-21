68e197ffbb79a6735e781e3c24b2f46b57b03866
Nick Schenone <nschenone16@gmail.com>
2023-06-21 14:19:31 -0700

    Changed creds from env vars to project secrets

--- src/ingest_and_serve.py
+++ src/ingest_and_serve.py
@@ -2,6 +2,16 @@ import os
 
 import mlrun
 from kfp import dsl
+from mlrun.runtimes.pod import KubeResource
+
+
+def apply_openai_env(project: mlrun.projects.MlrunProject, fn: KubeResource) -> None:
+    fn.set_env_from_secret(
+        "OPENAI_API_KEY", f"mlrun-project-secrets-{project.name}", "OPENAI_API_KEY"
+    )
+    fn.set_env_from_secret(
+        "OPENAI_API_BASE", f"mlrun-project-secrets-{project.name}", "OPENAI_API_BASE"
+    )
 
 
 @dsl.pipeline(name="LLM Pipeline")
@@ -14,23 +24,13 @@ def pipeline(
     # Ingest and index data in vector store
     ingest_fn = project.get_function("ingest-documents")
     ingest_fn.apply(mlrun.mount_v3io())
-    ingest_fn.set_envs(
-        {
-            "OPENAI_API_KEY": os.getenv("OPENAI_API_KEY"),
-            "OPENAI_API_BASE": os.getenv("OPENAI_API_BASE"),
-        }
-    )
+    apply_openai_env(project=project, fn=ingest_fn)
     ingest_run = project.run_function(ingest_fn, params={"persist_directory": persist_directory})
 
     # Serve LLM
     serving_fn = project.get_function("serve-llm")
     serving_fn.apply(mlrun.mount_v3io())
-    serving_fn.set_envs(
-        {
-            "OPENAI_API_KEY": os.getenv("OPENAI_API_KEY"),
-            "OPENAI_API_BASE": os.getenv("OPENAI_API_BASE"),
-        }
-    )
+    apply_openai_env(project=project, fn=serving_fn)
     graph = serving_fn.set_topology("flow", engine="async")
     graph.add_step(
         name="llm",
